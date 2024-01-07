8936f72a3f0d24a9dffd7d2d6556558d672e7e83
Armin Veres <armin.veres@hotmail.com>
2024-01-07 16:11:34 +0100

    fix: add const qualifier to summary print
    Fixes mutability error of compiler:
    `'this' argument to member function 'print_summary' has type 'const Particle3D', but function is not marked const`

--- exercises/n-body/include/particle.hpp
+++ exercises/n-body/include/particle.hpp
@@ -4,19 +4,20 @@
 #include <Eigen/Dense>
 #include <vector>
 
-
 class Particle3D {
   public:
-    double mass;
     Eigen::Vector3d position;
     Eigen::Vector3d velocity;
     double softening;
     double potential;
     double distance;
+    double mass;
 
-    /// @brief calculate the Norm of the position vector
+    /// Calculate the Norm of the position vector and set the value
     auto calc_orign_distance() -> void;
-		auto print_summary() -> void;
+
+    /// Print a summary of the particle properties
+    auto print_summary() const -> void;
 };
 
 /// Utility functions to do work with a `std::vector<Particle3D>`
@@ -26,6 +27,5 @@ namespace Particles {
 /// @note In a running system, the distances need to be calculated at each step
 auto get_max_distance(const std::vector<Particle3D> &particles) -> Particle3D;
 } // namespace Particles
-
 
 #endif // ! PARTICLES_H_
--- exercises/n-body/src/particle.cpp
+++ exercises/n-body/src/particle.cpp
@@ -1,11 +1,11 @@
 #include "particle.hpp"
-#include "logging.hpp"
 
 #include <algorithm>
+#include <iostream>
 
 auto Particle3D::calc_orign_distance() -> void { this->distance = this->position.norm(); }
 
-auto Particle3D::print_summary() -> void {
+auto Particle3D::print_summary() const -> void {
     std::cout << "Position = " << this->position << std::endl;
     std::cout << "Potential = " << this->potential << std::endl;
     std::cout << "Velocity = " << this->velocity << std::endl;
@@ -18,7 +18,7 @@ auto Particles::get_max_distance(const std::vector<Particle3D> &particles) -> Pa
             return a.distance < b.distance;
         });
 
-    Logging::info(result->position, "Position: ");
-    Logging::info(result->distance, "Distance: ");
+    result->print_summary();
+
     return *result;
 }
